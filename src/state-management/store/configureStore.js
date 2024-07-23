cd35da769b66dc721c316e1a9d850681e65981a6
Tanmoy <104419571+TanmoyTheStudent@users.noreply.github.com>
2024-07-23 23:20:50 +0530

    Update configureStore.js

--- src/state-management/store/configureStore.js
+++ src/state-management/store/configureStore.js
@@ -1,14 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import {thunk} from 'redux-thunk'
 import tweetsReducer from '../reducers/tweets-reducer'
-// import spacesReducer from '../reducers/spaces-reducer'
-// import bookingsReducer from '../reducers/bookings-reducer'
+
 
 const configureStore = () => {
     const store = createStore(combineReducers({
         tweets: tweetsReducer 
-        // spaces: spacesReducer,
-        // bookings:bookingsReducer
+     
     }), applyMiddleware(thunk))
     return store 
 }
